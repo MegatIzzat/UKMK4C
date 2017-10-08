8b1d0c0c24aedef91a713aa394b7f771c5a0d95c
MEiiYo <megat7996@gmail.com>
2017-10-09 03:33:13 +0800

    Updated migrations

--- database/migrations/2017_10_08_155638_category.php
+++ database/migrations/2017_10_08_155638_category.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class Category extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        //
+        Schema::create('category', function(Blueprint $table){
+            $table->string('category_id',5);
+            $table->string('category_name',30);
+
+            $table->primary('category_id');
         });
     }
 
@@ -30,6 +29,8 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        //
+        Schema::dropIfExists('category');
+
     }
 }
---
+++ database/migrations/2017_10_08_183213_orderline.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Orderline extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('orderline', function(Blueprint $table){
+            $table->string('order_id',5);
+            $table->string('product_id',5);
+            $table->integer('quantity');
+            $table->timestamp('order_date');
+            $table->float('total_price',5,2);
+
+            $table->foreign('order_id')->references('order_id')->on('order');
+            $table->foreign('product_id')->references('product_id')->on('product');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+                Schema::dropIfExists('orderline');
+
+    }
+}
