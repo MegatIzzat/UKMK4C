c12365ab0221824839aa2f8fe25a1ffd3bb0a850
MEiiYo <megat7996@gmail.com>
2017-10-10 17:45:00 +0800

    Added Form facade

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        // $this->call(ProductSeeder::class);
-        // $this->call(CategorySeeder::class);
     }
 }
