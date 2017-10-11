e5c1c7e3aeabc5a543b9590960e09d34445305c1
ndeakhrdn <illinadiaa@gmail.com>
2017-10-12 04:21:05 +0800

    Product Image

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -22,18 +22,17 @@
 				@foreach($product as $p)
 			  		<div class="col-md-4 col-xs-12 ">
 						<div class="thumbnail">
-							<a href="#"><img src="{{asset('img/P0001.jpg')}}" alt="Product-img"></a>
+								<a href="#"><img class="img-responsive" src="{{asset('img/'.$p->product_id.'.jpg')}}" alt="product-img"></a>
 								<div class="caption">
 									<h4><a href="#">{{$p->product_name}}</a></h4>
 									<h5>RM {{$p->product_price}}</h5>
 								</div>
 								<div class="footer">
 								
-									<a href="{{route('product.addToCart',['product_id' => $p->product_id])}}" class="btn btn-success pull-right" role="button">Add to Cart</a>
+									<a href="{{route('product.addToCart',['product_id' => $p->product_id])}}" class="btn btn-success" role="button">Add to Cart</a>
 								
 									
 								</div>
-								
 						</div>
 					</div>
 				@endforeach
