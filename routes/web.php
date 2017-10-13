17c0257b81c52058aec99fc443d0492fc86b18c3
MEiiYo <megat7996@gmail.com>
2017-10-13 16:56:00 +0800

    Product CRUD v3.1

--- routes/web.php
+++ routes/web.php
@@ -60,32 +60,50 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 use Illuminate\Http\Request;
 
-Route::get('productajaxCRUD', function () {
-    $products = App\Product::all();
-    return view('product.index')->with('products',$products);
-});
-Route::get('productajaxCRUD/{product_id?}',function($product_id){
-    $product = App\Product::find($product_id);
-    return response()->json($product);
-});
-Route::post('productajaxCRUD',function(Request $request){   
-    $product = App\Product::create($request->input());
-    return response()->json($product);
-});
-Route::put('productajaxCRUD/{product_id?}',function(Request $request,$product_id){
-    $product = App\Product::find($product_id);
-    $product->product_name = $request->product_name;
-    $product->product_price= $request->product_price;
-    $product->category_id= $request->category_id;
-    $product->product_img= $request->product_img;
-    $product->product_rating= $request->product_rating;
-    $product->save();
-    return response()->json($product);
-});
-Route::delete('productajaxCRUD/{product_id?}',function($product_id){
-    $product = App\Product::destroy($product_id);
-    return response()->json($product);
-});
+Route::get('/productmanagement', 'ProductController@index')->name('index');
+
+Route::get('/productmanagement/{product_id?}', 'ProductController@show')->name('show');
+
+Route::post('/productmanagement', 'ProductController@create')->name('create');
+// Route::post('/productmanagement', 'ProductController@store')->name('store');
+
+Route::put('/productmanagement/{product_id?}', 'ProductController@update')->name('update');
+
+Route::delete('/productmanagement/{product_id?}', 'ProductController@destroy')->name('delete');
+
+// Route::get('form-validation', 'HomeController@formValidation');
+// Route::post('form-validation', 'HomeController@formValidationPost');
+
+// Route::get('productajaxCRUD', function () {
+//     $products = App\Product::all();
+//     return view('product.index')->with('products',$products);
+// });
+// Route::get('productajaxCRUD/{product_id?}',function($product_id){
+//     $product = App\Product::find($product_id);
+//     return response()->json($product);
+// });
+// Route::post('productajaxCRUD',function(Request $request){   
+//     $product = App\Product::create($request->input());
+//     return response()->json($product);
+// });
+// Route::put('productajaxCRUD/{product_id?}',function(Request $request,$product_id){
+//     $product = App\Product::find($product_id);
+//     $product->product_name = $request->product_name;
+//     $product->product_price= $request->product_price;
+//     $product->category_id= $request->category_id;
+//     $product->product_img= $request->product_img;
+//     $product->product_rating= $request->product_rating;
+//     $product->save();
+//     return response()->json($product);
+// });
+// Route::delete('productajaxCRUD/{product_id?}',function($product_id){
+//     $product = App\Product::destroy($product_id);
+//     return response()->json($product);
+// });
+
+
+/*------------------------ CART -----------------------------*/
+
 
 Route::get('add-to-cart/{product_id}', [
 	'uses' => 'CustomerController@AddToCart',
