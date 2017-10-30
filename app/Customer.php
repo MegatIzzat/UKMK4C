90754fd924764303aa44e9def04bb5f2ad832c71
MEiiYo <megat7996@gmail.com>
2017-10-30 22:05:38 +0800

    minor

--- app/Customer.php
+++ app/Customer.php
@@ -11,7 +11,7 @@ class Customer extends Model
     protected $fillable = ['cust_id','cust_name','cust_pass','cust_balance'];
     protected $primaryKey = 'cust_id';
 
-    public $timestamp = false;
+    public $timestamps = false;
 
     public function Topup(){
     	return $this->hasMany('App\Topup','cust_id');
