c326dc8bc72dd84f8736ba1287c7074b07810ea7
MEiiYo <megat7996@gmail.com>
2017-11-02 19:37:15 +0800

    revert

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -12,49 +12,49 @@ class UserSeeder extends Seeder
     public function run()
     {
          App\User::insert([
-         		// [
-           //          'user_id' => 'admin',
-           //          'name' => 'Fattah Amin',
-           //          'email' => 'fattah@gmail.com',
-           //          'password' => '1234',
+         		[
+                    'user_id' => 'admin',
+                    'name' => 'Fattah Amin',
+                    'email' => 'fattah@gmail.com',
+                    'password' => '1234',
                     
-           //      ],
+                ],
 
-           //      [
-           //          'user_id' => 'S0002',
-           //          'name' => 'Zizan Razak',
-           //          'email' => 'zizan@gmail.com',
-           //          'password' => '1234',
-           //      ],
+                [
+                    'user_id' => 'S0002',
+                    'name' => 'Zizan Razak',
+                    'email' => 'zizan@gmail.com',
+                    'password' => '1234',
+                ],
 
-           //      [
-           //          'user_id' => 'S0003',
-           //          'name' => 'Zaiton Sameon',
-           //          'email' => 'zaiton@gmail.com',
-           //          'password' => '1234',
-           //      ],
+                [
+                    'user_id' => 'S0003',
+                    'name' => 'Zaiton Sameon',
+                    'email' => 'zaiton@gmail.com',
+                    'password' => '1234',
+                ],
 
-           //      [
-           //          'user_id' => 'C0001',
-           //          'name' => 'Awaluddin Alimuddin',
-           //          'email' => 'awaluddin@gmail.com',
-           //          'password' => '1234',
+                [
+                    'user_id' => 'C0001',
+                    'name' => 'Awaluddin Alimuddin',
+                    'email' => 'awaluddin@gmail.com',
+                    'password' => '1234',
                     
-           //      ],
+                ],
 
-           //      [
-           //          'user_id' => 'C0002',
-           //          'name' => 'Ashraf Razlan',
-           //          'email' => 'ashraf@gmail.com',
-           //          'password' => '1234',
-           //      ],
+                [
+                    'user_id' => 'C0002',
+                    'name' => 'Ashraf Razlan',
+                    'email' => 'ashraf@gmail.com',
+                    'password' => '1234',
+                ],
 
-           //      [
-           //          'user_id' => 'C0003',
-           //          'name' => 'Syafiqah Salim',
-           //          'email' => 'syafiqah@gmail.com',
-           //          'password' => '1234',
-           //      ]
+                [
+                    'user_id' => 'C0003',
+                    'name' => 'Syafiqah Salim',
+                    'email' => 'syafiqah@gmail.com',
+                    'password' => '1234',
+                ]
 
         ]);
     }
