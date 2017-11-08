6c50d9f12be240253108ad5e9466ceb67118660d
MEiiYo <megat7996@gmail.com>
2017-11-08 10:51:00 +0800

    minor

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,6 +62,11 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
         ],
+        
+        'upload' => [
+            'driver' => 'local',
+            'root' => public_path() . '/img',
+        ],
 
     ],
 
