1e416048abbcf5a80910a41e1f1ba6d5554cc9a9
MEiiYo <megat7996@gmail.com>
2017-11-08 10:47:46 +0800

    minor

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,45 +5,46 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Product;
 use App\Category;
+use Storage;
 use Validator;
+
 
 class ProductController extends Controller
 {
 
     public function index()
     {
-    	$products = Product::all();
+        $products = Product::all();
         $category=Category::all();
-        return view('staff.product.index', compact('category'))->with('products',$products);
-    	// return view('product.index')->with('products',$products);
+        return view('product.index', compact('category'))->with('products',$products);
+        // return view('product.index')->with('products',$products);
     }
 
     public function show($product_id)
     {
-    	$product = Product::find($product_id);
-    	return response()->json($product);	
+        $product = Product::find($product_id);
+        return response()->json($product);  
     }
 
 
     public function create(Request $request)
     {
-    	
-      $product = Product::create($request->input());
-    	return response()->json($product);
+        $product = Product::create($request->input());
+        return response()->json($product);
     }
 
     public function update(Request $request,$product_id)
     {
-    	// $product = Product::find($product_id)->update([
-	    // $product->product_name = $request->upproduct_name,
-	    // $product->product_price= $request->upproduct_price,
-	    // $product->category_id= $request->upcategory_id,
-	    // $product->product_img= $request->upproduct_img,
-	    // $product->product_rating= $request->upproduct_rating,
+        // $product = Product::find($product_id)->update([
+        // $product->product_name = $request->upproduct_name,
+        // $product->product_price= $request->upproduct_price,
+        // $product->category_id= $request->upcategory_id,
+        // $product->product_img= $request->upproduct_img,
+        // $product->product_rating= $request->upproduct_rating,
      //    ]);
 
-	    // $product->save();
-	    // return response()->json($product);
+        // $product->save();
+        // return response()->json($product);
         Validator::make($request->all(), [
             'upproduct_id' => 'required',
             'upproduct_name' => 'required',
@@ -65,34 +66,29 @@ class ProductController extends Controller
 
             ]);
         
-        return redirect()->route('staff.product.index');
+        return redirect()->route('staff.product.index')->with('success', $id.' has been successfully updated!');
     }
 
-    public function destroy($product_id)
+    public function destroy(Request $request, $product_id)
     {
-    	$product = Product::destroy($product_id);
-    	return response()->json($product);
+        $id = $request->deproduct_id;
+
+        $product = Product::destroy($id);
+        return redirect()->route('staff.product.index')->with('success', $id.' has been successfully deleted!');
     }
 
-    // public function upload(Request $request, $product_id)
-    // {
-    //   $validator = Validator::make($request->all(), [
-    //     'product_img' => 'required|image',
-    //   ]
-    //   );
+    public function upload(Request $request)
+    {
+        $product_id = $request->fileproduct_id;
+        $file = $request->file('fileproduct_img');
+        $filename = $file->getClientOriginalName();
+        $product = \DB::table('product')->where('product_id', $product_id)->update(['product_img' => $filename]);
+        if (!empty($file)){
+            Storage::disk('upload')->put($file->getClientOriginalName(), file_get_contents($file));
+        }
 
-    //   if ($validator->passes()) {
+        return redirect()->route('staff.product.index')->with('success', $filename.' has been successfully uploaded!');
 
-    //     $input = $request->all();
-    //     $input['product_img'] = time().'.'.$request->product_img->getClientOriginalExtension();
-    //     $request->product_img->move(public_path('img'), $input['product_img']);
-
-    //     $img = \DB::table('product')->where('product_id', $product_id)->update(['product_img' => $input]);
-
-    //     return response()->json(['success'=>'done']);
-    //   }
-
-    //   return response()->json(['error'=>$validator->errors()->all()]);
-    // }
+    }
 
 }
