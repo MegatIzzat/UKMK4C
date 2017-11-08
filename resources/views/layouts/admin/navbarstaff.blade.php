c42ac02f012aa21201ec1b5d9fe3e25a352df7e5
ndeakhrdn <illinadiaa@gmail.com>
2017-11-09 05:13:58 +0800

    navbar

--- resources/views/layouts/admin/navbarstaff.blade.php
+++ resources/views/layouts/admin/navbarstaff.blade.php
@@ -42,7 +42,14 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                            
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true"> List <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href=" {{url('/staff/customer')}} "> Customer</a></li>
+                                <li><a href="">Staff</a></li>
+                            </ul>
+                        </li>
+
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
