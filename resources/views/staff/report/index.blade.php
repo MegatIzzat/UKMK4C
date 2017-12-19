2623dd385b7abd45c656f511253778126745e87e
MEiiYo <megat7996@gmail.com>
2017-12-20 01:21:32 +0800

    sales report

--- resources/views/staff/report/index.blade.php
+++ resources/views/staff/report/index.blade.php
@@ -13,13 +13,13 @@
 
             #content
             {
-                position:absolute; left: 0; right: 0; bottom: 0; top: 0px; 
+                position:absolute; left: 0; right: 0; bottom: 0; top: 60px; 
             }
         </style>
     </head>
     <body>
         <div id="content">
-            <iframe width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiY2ZmOWEwMDEtNmQzOS00OTY4LWE2YzMtNWQ4YjU0ZjEwYmZhIiwidCI6IjdhODU3ZTA5LWQ5YWQtNDNkMi04OTNlLTMyMTVkZGRkM2EzYiIsImMiOjEwfQ%3D%3D" frameborder="1" allowFullScreen="true" align="middle"></iframe>
+            <iframe width="100%" height="100%" src="https://app.powerbi.com/view?r=eyJrIjoiODE4N2NkMjUtNTA2Yy00NjUxLTgyYmEtNzYxYzU2ZDU4MjBkIiwidCI6IjdhODU3ZTA5LWQ5YWQtNDNkMi04OTNlLTMyMTVkZGRkM2EzYiIsImMiOjEwfQ%3D%3D" frameborder="1" allowFullScreen="true" align="middle"></iframe>
         </div>
     </body>
 
