d0e4a95f6d8c4cb81fef1e10bf992b76d73d00fc
FaizSE <32590780+FaizSE@users.noreply.github.com>
2017-12-21 07:15:13 +0800

    minor

--- resources/views/layouts/admin/navbarstaff.blade.php
+++ resources/views/layouts/admin/navbarstaff.blade.php
@@ -61,7 +61,7 @@
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                   <span class="glyphicon glyphicon-user"></span> &nbsp;{{ Auth::user()->user_name }} <span class="caret"></span>
+                                   <span class="glyphicon glyphicon-briefcase"></span> &nbsp;{{ Auth::user()->user_name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
