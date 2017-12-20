472610412d4eab1f5ede4f6a24f0cda1e0579a67
FaizSE <32590780+FaizSE@users.noreply.github.com>
2017-12-21 01:21:58 +0800

    manage profile

--- resources/views/customer/profile.blade.php
+++ resources/views/customer/profile.blade.php
@@ -10,32 +10,41 @@
 				
 
 				<div class="page-header">
-					<h2>My Profile</h2>
+					<h2><center>MANAGE PROFILE</h2><br><br>
 
 				<form class="form-horizontal" method="POST" action="{{route('cust.profile.update',['user' => Auth::user()->user_id] )}}">
 				{{csrf_field()}}
 				{{method_field('PUT')}}
 
 					<div class="form-group">
-						<label for="user_id" class="col-sm-3 control-label">Customer ID</label>
+						<label for="user_id" class="col-sm-3 control-label"><b>User ID</b></label>
 						<div class="col-sm-9">
-							<input name="user_id" type="text" class="form-control" id="user_id" value="{{Auth::user()->user_id}}" readonly>
+							<label for="user_id" class="control-label">{{Auth::user()->user_id}}</label>
+							<input name="user_id" type="hidden" id="user_id" value="{{Auth::user()->user_id}}">
 						</div>
 					</div>
+
 					<div class="form-group">
-						<label for="name" class="col-sm-3 control-label">Customer Name</label>
+						<label for="cust_balance" class="col-sm-3 control-label"><b>Balance</b></label>
+						<div class="col-sm-9">
+							<label for="cust_balance" class="control-label">RM {{number_format($customer->cust_balance, 2)}}</label>
+						</div>
+					</div>
+
+					<div class="form-group">
+						<label for="name" class="col-sm-3 control-label"><b>Name</b></label>
 						<div class="col-sm-9">
 							<input name="name" type="text" class="form-control" id="name" value=" {{Auth::user()->user_name}} " required>
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="email" class="col-sm-3 control-label">Customer Email</label>
+						<label for="email" class="col-sm-3 control-label"><b>Email</b></label>
 						<div class="col-sm-9">
 							<input name="email" type="email" class="form-control" id="email" value="{{$customer->cust_email}}" required>
 						</div>
 					</div>
 					<div class="form-group">
-						<label for="cust_pass" class="col-sm-3 control-label">Password</label>
+						<label for="cust_pass" class="col-sm-3 control-label"><b>Password</b></label>
 						<div class="col-sm-9">
 							<input name="password" type="password" class="form-control" id="password"  value="{{$user->password}}" required>
 						</div>
@@ -59,38 +68,6 @@
 				</div>
 			
 			
-		</div>
-		<div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<div class="page-header">
-					<h2>My balance</h2>
-				</div>
-				<form action="update" method="post" class="form-horizontal">
-					<div class="form-group">
-						<label for="cust_balance" class="col-sm-3 control-label">Balance (RM)</label>
-						<div class="col-sm-9">
-							<input name="cust_balance" type="text" class="form-control" id="cust_balance" value= "{{number_format($customer->cust_balance, 2)}}" readonly="">
-						</div>
-					</div>
-				</form>
-				<div class="form-group">
-						
-		</div> </div>
-
-		<!-- <div class="row">
-			<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-				<div class="page-header">
-
-					<h2>Order History</h2>
-					</div>
-					<div class="form-group">
-						<div class="col-sm-12">
-							<button class="btn btn-warning btn-block" type="submit" name="update" >View Order History</button>
-						</div>
-					</div>
-			
-		</div>
-		</div> -->
 		</div>
 
 @endsection
